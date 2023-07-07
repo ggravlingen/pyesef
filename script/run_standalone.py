3ead0f6c88234d297ef7a03157ca1c3d2a4d2f47
“Patrik <21142447+ggravlingen@users.noreply.github.com>
2023-07-07 18:25:20 +0000

    Fix ruff

--- script/run_standalone.py
+++ script/run_standalone.py
@@ -6,6 +6,6 @@ import sys
 folder = os.path.dirname(__file__)
 sys.path.insert(0, os.path.normpath(f"{folder}/.."))
 
-from pyesef.helpers.read_and_save_filings import read_and_save_filings
+from pyesef.helpers.read_and_save_filings import read_and_save_filings  # noqa: E402
 
 read_and_save_filings()
