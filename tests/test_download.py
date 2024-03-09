b64ace091a55d9ca4e9d1ca73facb8c25e8e762c
Patrik <21142447+ggravlingen@users.noreply.github.com>
2024-03-09 13:28:06 +0000

    Fix

--- tests/test_download.py
+++ tests/test_download.py
@@ -89,10 +89,10 @@ def test_filing_property__file_url() -> None:
 def test_filing_property__download_country_folder() -> None:
     """Return Filing.download_country_folder."""
     model = Filing(country="se", file_name="abc", path="a/b/c")
-    assert model.download_country_folder == "/workspaces/pyesef/archives/se"
+    assert "/pyesef/archives/se" in model.download_country_folder
 
 
 def test_filing_property__write_location() -> None:
     """Return Filing.write_location."""
     model = Filing(country="se", file_name="abc", path="a/b/c")
-    assert model.write_location == "/workspaces/pyesef/archives/se/abc"
+    assert "/pyesef/archives/se/abc" in model.write_location
