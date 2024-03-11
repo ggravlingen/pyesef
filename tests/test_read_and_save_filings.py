689583c5b9d98322f9a94d7993cfb0422fb83971
Patrik <21142447+ggravlingen@users.noreply.github.com>
2024-03-11 18:18:45 +0000

    Add test

--- tests/test_read_and_save_filings.py
+++ tests/test_read_and_save_filings.py
@@ -1,8 +1,13 @@
 """Tests for read and save filings."""
 
 from datetime import date
+import os
+from unittest.mock import patch
 
-from pyesef.helpers.read_and_save_filings import data_list_to_clean_df
+from pyesef.helpers.read_and_save_filings import (
+    data_list_to_clean_df,
+    read_and_save_filings,
+)
 from pyesef.load_parse_file.common import EsefData
 
 
@@ -43,3 +48,14 @@ def test_data_list_to_clean_df__drop_duplicates() -> None:
         ]
     )
     assert len(function_result) == 1
+
+
+def test_read_and_save_filings() -> None:
+    """Test read_and_save_filings."""
+    with patch(
+        "pyesef.helpers.read_and_save_filings.PATH_ARCHIVES",
+        os.path.abspath(os.path.join("tests", "fixtures")),
+    ):
+        read_and_save_filings(move_parsed_file=False)
+
+        assert True
