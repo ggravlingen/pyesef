66a71ac2ecb05aef7e0d45b9191e2ba6af7e6045
Patrik <21142447+ggravlingen@users.noreply.github.com>
2024-03-17 18:47:41 +0000

    Deprecate hierarchy

--- tests/test_read_and_save_filings.py
+++ tests/test_read_and_save_filings.py
@@ -4,120 +4,12 @@ from datetime import date
 import os
 from unittest.mock import patch
 
-import pytest
-
 from pyesef.parse_xbrl_file.common import EsefData
-from pyesef.parse_xbrl_file.hierarchy import Hierarchy
 from pyesef.parse_xbrl_file.read_and_save_filings import (
     ReadFiling,
     data_list_to_clean_df,
 )
 from pyesef.parse_xbrl_file.save_excel import SaveToExcel
-
-HIERARCHY_TEST_DATA = {
-    "AdjustmentsForDecreaseIncreaseInTradeAndOtherReceivables": (
-        "CashFlowsFromUsedInOperatingActivities"
-    ),
-    "AdjustmentsForIncreaseDecreaseInTradeAndOtherPayables": (
-        "CashFlowsFromUsedInOperatingActivities"
-    ),
-    "CashAndCashEquivalents": "CurrentAssets",
-    "CashFlowsFromUsedInFinancingActivities": (
-        "IncreaseDecreaseInCashAndCashEquivalents"
-    ),
-    "CashFlowsFromUsedInInvestingActivities": (
-        "IncreaseDecreaseInCashAndCashEquivalents"
-    ),
-    "CashFlowsFromUsedInOperatingActivities": (
-        "IncreaseDecreaseInCashAndCashEquivalents"
-    ),
-    "CashFlowsFromUsedInOperationsBeforeChangesInWorkingCapital": (
-        "CashFlowsFromUsedInOperatingActivities"
-    ),
-    "CurrentAccruedExpensesAndOtherCurrentLiabilities": "CurrentLiabilities",
-    "CurrentAssets": "Assets",
-    "CurrentFinancialAssetsAtFairValueThroughProfitOrLoss": "CurrentAssets",
-    "CurrentLiabilities": "EquityAndLiabilities",
-    "CurrentPrepaidExpenses": "CurrentAssets",
-    "DeferredTaxExpenseIncomeRelatingToOriginationAndReversalOfTemporaryDifferences": (
-        "ProfitLoss"
-    ),
-    "DeferredTaxLiabilities": "NoncurrentLiabilities",
-    "DirectOperatingExpenseFromInvestmentProperty": (
-        "RentalIncomeFromInvestmentPropertyNetOfDirectOperatingExpense"
-    ),
-    "DividendsPaidClassifiedAsFinancingActivities": (
-        "CashFlowsFromUsedInFinancingActivities"
-    ),
-    "DividendsReceivedClassifiedAsOperatingActivities": (
-        "CashFlowsFromUsedInOperationsBeforeChangesInWorkingCapital"
-    ),
-    "Equity": "EquityAndLiabilities",
-    "FinanceCosts": "ResultatEfterFinansiellaKostnader",
-    "FinanceIncome": "ResultatEfterFinansiellaKostnader",
-    "Forvaltningsresultat": (
-        "CashFlowsFromUsedInOperationsBeforeChangesInWorkingCapital"
-    ),
-    "ForvaltningsresultatFranIntresseforetagOchJointVenture": (
-        "CashFlowsFromUsedInOperationsBeforeChangesInWorkingCapital"
-    ),
-    "GainsLossesOnChangeInFairValueOfDerivatives": "ProfitLossBeforeTax",
-    "GeneralAndAdministrativeExpense": "ResultatEfterFinansiellaKostnader",
-    "IncomeTaxExpenseContinuingOperations": "ProfitLoss",
-    "IncomeTaxesPaidClassifiedAsOperatingActivities": (
-        "CashFlowsFromUsedInOperationsBeforeChangesInWorkingCapital"
-    ),
-    "InvesteringarIBefintligaFastigheterOchOvrigaAnlaggningstillgangar": (
-        "CashFlowsFromUsedInInvestingActivities"
-    ),
-    "InvesteringarINybyggnation": "CashFlowsFromUsedInInvestingActivities",
-    "InvestmentProperty": "NoncurrentAssets",
-    "InvestmentsInSubsidiariesJointVenturesAndAssociates": "NoncurrentAssets",
-    "LongtermBorrowings": "NoncurrentLiabilities",
-    "NoncurrentAssets": "Assets",
-    "NoncurrentDerivativeFinancialLiabilities": "NoncurrentLiabilities",
-    "NoncurrentLeaseLiabilities": "NoncurrentLiabilities",
-    "NoncurrentLiabilities": "EquityAndLiabilities",
-    "NoncurrentReceivables": "NoncurrentAssets",
-    "OtherAdjustmentsForNoncashItems": (
-        "CashFlowsFromUsedInOperationsBeforeChangesInWorkingCapital"
-    ),
-    "OtherComprehensiveIncome": "ComprehensiveIncome",
-    "OtherCurrentNonfinancialLiabilities": "CurrentLiabilities",
-    "OtherNoncurrentAssets": "NoncurrentAssets",
-    "OtherNoncurrentFinancialAssets": "NoncurrentAssets",
-    "ProceedsFromBorrowingsClassifiedAsFinancingActivities": (
-        "CashFlowsFromUsedInFinancingActivities"
-    ),
-    "ProceedsFromIssuingShares": "CashFlowsFromUsedInFinancingActivities",
-    "ProceedsFromSalesOfInvestmentProperty": "CashFlowsFromUsedInInvestingActivities",
-    "ProfitLoss": "ComprehensiveIncome",
-    "ProfitLossBeforeTax": "ProfitLoss",
-    "PropertyTaxExpense": (
-        "RentalIncomeFromInvestmentPropertyNetOfDirectOperatingExpense"
-    ),
-    "PurchaseOfFinancialInstrumentsClassifiedAsInvestingActivities": (
-        "CashFlowsFromUsedInInvestingActivities"
-    ),
-    "PurchaseOfInvestmentProperty": "CashFlowsFromUsedInInvestingActivities",
-    "RentalIncomeFromInvestmentProperty": (
-        "RentalIncomeFromInvestmentPropertyNetOfDirectOperatingExpense"
-    ),
-    "RentalIncomeFromInvestmentPropertyNetOfDirectOperatingExpense": (
-        "ResultatEfterFinansiellaKostnader"
-    ),
-    "RepaymentsOfBorrowingsClassifiedAsFinancingActivities": (
-        "CashFlowsFromUsedInFinancingActivities"
-    ),
-    "ResultatEfterFinansiellaKostnader": "ProfitLossBeforeTax",
-    "RightofuseAssetsThatDoNotMeetDefinitionOfInvestmentProperty": "NoncurrentAssets",
-    "ShareOfProfitLossOfAssociatesAndJointVenturesAccountedForUsingEquityMethod": (
-        "ResultatEfterFinansiellaKostnader"
-    ),
-    "ShorttermBorrowings": "CurrentLiabilities",
-    "TradeAndOtherCurrentReceivables": "CurrentAssets",
-    "VardeforandringarForvaltningsfastigheter": "ProfitLossBeforeTax",
-}
 
 
 def test_data_list_to_clean_df__drop_duplicates() -> None:
@@ -163,24 +55,3 @@ def test_read_and_save_filings() -> None:
     ):
         ReadFiling(should_move_parsed_file=False)
         assert os.path.exists(SaveToExcel.TEMPLATE_OUTPUT_PATH_EXCEL)
-
-
-@pytest.mark.parametrize(
-    "xml_name, parent_node",
-    [
-        ("GeneralAndAdministrativeExpense", "ComprehensiveIncome"),
-        ("CashAndCashEquivalents", "Assets"),
-        (
-            "RepaymentsOfBorrowingsClassifiedAsFinancingActivities",
-            "IncreaseDecreaseInCashAndCashEquivalents",
-        ),
-    ],
-)
-def test_hierarchy_function(xml_name, parent_node) -> None:
-    """Test node functionality."""
-    hierarchy = Hierarchy(hierarchy_dict=HIERARCHY_TEST_DATA)
-
-    ultimate_parent = hierarchy.get_ultimate_parent(xml_name)
-
-    assert ultimate_parent is not None
-    assert ultimate_parent.name == parent_node
