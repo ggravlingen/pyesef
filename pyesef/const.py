38164ec25aee8a33da922c41f9ed53dcd46010bd
Patrik Lindgren <21142447+ggravlingen@users.noreply.github.com>
2024-03-18 19:56:04 +0100

    Add more taxonomies data (#446)

--- pyesef/const.py
+++ pyesef/const.py
@@ -8,6 +8,7 @@ import pathlib
 
 PATH_BASE = pathlib.Path(__file__).parent.resolve()
 PATH_PROJECT_ROOT = os.path.abspath(os.path.join(PATH_BASE, ".."))
+PATH_STATIC = os.path.join(PATH_PROJECT_ROOT, "pyesef", "static")
 
 
 class NiceType(StrEnum):
